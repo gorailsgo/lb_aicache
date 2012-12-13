ca0d2efbd14ae16fde2490038268d76181a97ad8
root <root@ubuntuserver64.(none)>
2012-12-13 17:07:23 +0200

    Added a hello world message to the default.rb recipe

---
+++ cookbooks/aicache-test/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "aiCache"
+maintainer_email "roman@aicache.com"
+license          "All rights reserved"
+description      "Installs/Configures aicache-test"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+recipe "aicache-test:default","Prints Hello World output"
---
+++ cookbooks/aicache-test/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: aicache-test
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+log "Hello from aiCache!"
