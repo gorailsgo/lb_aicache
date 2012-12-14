ae51bfa537fada59f77148c3ab4c5448f229a426
root <root@ubuntuserver64.(none)>
2012-12-14 16:27:57 +0200

    Corrected an error in metadata.rb file

--- cookbooks/aicache-test/metadata.rb
+++ cookbooks/aicache-test/metadata.rb
@@ -5,4 +5,4 @@ description      "Installs/Configures aicache-test"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
-recipe "aicache-test:default","Prints Hello World output"
+recipe "aicache-test::default","Prints Hello World output"
