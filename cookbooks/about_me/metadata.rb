10114aa106e6658a874afff266a06ff5480adab8
Roman D <roman@aicache.com>
2012-12-21 12:20:37 +0200

    about me test

---
+++ cookbooks/about_me/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "aiCache"
+maintainer_email "roman@aicache.com"
+license          "All rights reserved"
+description      "Simple about_me recipe"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+recipe "about_me::default","Prints my first name and several of my favorite things as output."
