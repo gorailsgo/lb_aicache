303145e3b3e686b2cf76306d5e1d956183f19ea6
Roman D <roman@aicache.com>
2013-01-09 15:03:03 +0200

    removed test files

--- cookbooks/about_me/attributes/me.rb
+++
@@ -1,4 +0,0 @@
-set[:about_me][:first_name] = "Roman"
-default[:about_me][:favorite][:food] = "pizza"
-default[:about_me][:favorite][:drink] = "water"
-default[:about_me][:favorite][:hobby] = "running"
