79090c0e86f18137d07a85110965558343e56633
Roman D <roman@aicache.com>
2013-01-09 11:26:03 +0200

    no need for variables for the default aicache config

--- cookbooks/lb_aicache/attributes/default.rb
+++ cookbooks/lb_aicache/attributes/default.rb
@@ -3,7 +3,7 @@
 #
 
 # Set defaults for aicache specific configuration items
-set_unless[:lb_aicache][:algorithm] = 'roundrobin'
-set_unless[:lb_aicache][:timeout_client] = '60000'
-set_unless[:lb_aicache][:timeout_server] = '60000'
+#set_unless[:lb_aicache][:algorithm] = 'roundrobin'
+#set_unless[:lb_aicache][:timeout_client] = '60000'
+#set_unless[:lb_aicache][:timeout_server] = '60000'
 
