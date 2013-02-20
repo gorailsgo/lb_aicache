1453bd9efeac5bd099b1b36c5840e5e5d3ad5d50
Roman D <roman@aicache.com>
2013-02-20 16:51:16 +0200

    changed readme and metadata

--- cookbooks/lb_aicache/metadata.rb
+++ cookbooks/lb_aicache/metadata.rb
@@ -14,31 +14,3 @@ depends "app"
 depends "lb"
 
 recipe "lb_aicache::setup_server", "This loads the required 'lb' resource using the aiCache provider."
-
-attribute "lb_aicache/algorithm",
-  :display_name => "Load Balancing Algorithm",
-  :description => "The algorithm that the load balancer will use to direct traffic.",
-  :required => "optional",
-  :default => "roundrobin",
-  :choice => ["roundrobin", "leastconn", "source"],
-  :recipes => [
-    "lb_aicache::setup_server"
-  ]
-
-attribute "lb_aicache/timeout_server",
-  :display_name => "Server Timeout",
-  :description => "The maximum inactivity time on the server side in seconds.",
-  :required => "optional",
-  :default => "60",
-  :recipes => [
-    "lb_aicache::setup_server"
-  ]
-
-attribute "lb_aicache/timeout_client",
-  :display_name => "Client Timeout",
-  :description => "The maximum inactivity time on the client side in seconds.",
-  :required => "optional",
-  :default => "60",
-  :recipes => [
-    "lb_aicache::setup_server"
-  ]
