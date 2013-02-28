318295e6fe942f4492f15579b7af7e01ad25f58d
Roman D <roman@aicache.com>
2013-02-28 17:06:37 +0200

    collectd .

--- cookbooks/lb_aicache/files/default/aicache_collectd.sh
+++ cookbooks/lb_aicache/files/default/aicache_collectd.sh
@@ -8,27 +8,27 @@ while sleep 10; do
         STATS=`tail --lines=1 $STATS_FILE`
         for STAT in $STATS; do
                 if   [ "$PREV_STAT" == "R/S:"   ] ; then
-                        echo "PUTVAL \"$INSTANCE_ID/aicache/gauge-requests_per_sec\" interval=10 N:$STAT"
+                        echo "PUTVAL \"$INSTANCE_UUID/aicache/gauge-requests_per_sec\" interval=10 N:$STAT"
                 elif [ "$PREV_STAT" == "CC#:"   ] ; then
-                        echo "PUTVAL \"$INSTANCE_ID/aicache/gauge-total_client_conns\" interval=10 N:$STAT"
+                        echo "PUTVAL \"$INSTANCE_UUID/aicache/gauge-total_client_conns\" interval=10 N:$STAT"
                 elif [ "$PREV_STAT" == "OC#:"   ] ; then
-                        echo "PUTVAL \"$INSTANCE_ID/aicache/gauge-total_origin_conns\" interval=10 N:$STAT"
+                        echo "PUTVAL \"$INSTANCE_UUID/aicache/gauge-total_origin_conns\" interval=10 N:$STAT"
                 elif [ "$PREV_STAT" == "OCR#:"  ] ; then
-                        echo "PUTVAL \"$INSTANCE_ID/aicache/gauge-overal_caching_ratio\" interval=10 N:$STAT"
+                        echo "PUTVAL \"$INSTANCE_UUID/aicache/gauge-overal_caching_ratio\" interval=10 N:$STAT"
                 elif [ "$PREV_STAT" == "SCR:" ] ; then
-                        echo "PUTVAL \"$INSTANCE_ID/aicache/gauge-specific_caching_ratio\" interval=10 N:$STAT"
+                        echo "PUTVAL \"$INSTANCE_UUID/aicache/gauge-specific_caching_ratio\" interval=10 N:$STAT"
                 elif [ "$PREV_STAT" == "ART:"   ] ; then
-                        echo "PUTVAL \"$INSTANCE_ID/aicache/gauge-origin_avg_response_time\" interval=10 N:$STAT"
+                        echo "PUTVAL \"$INSTANCE_UUID/aicache/gauge-origin_avg_response_time\" interval=10 N:$STAT"
                 elif [ "$PREV_STAT" == "BRq/S:" ] ; then
-                        echo "PUTVAL \"$INSTANCE_ID/aicache/gauge-bad_requests_per_sec\" interval=10 N:$STAT"
+                        echo "PUTVAL \"$INSTANCE_UUID/aicache/gauge-bad_requests_per_sec\" interval=10 N:$STAT"
                 elif [ "$PREV_STAT" == "BRp/S:" ] ; then
-                        echo "PUTVAL \"$INSTANCE_ID/aicache/gauge-bad_responses_per_sec\" interval=10 N:$STAT"
+                        echo "PUTVAL \"$INSTANCE_UUID/aicache/gauge-bad_responses_per_sec\" interval=10 N:$STAT"
                 elif [ "$PREV_STAT" == "CR#":   ] ; then
-                        echo "PUTVAL \"$INSTANCE_ID/aicache/gauge-total_cached_responses\" interval=10 N:$STAT"
+                        echo "PUTVAL \"$INSTANCE_UUID/aicache/gauge-total_cached_responses\" interval=10 N:$STAT"
                 elif [ "$PREV_STAT" == "BIB:"   ] ; then
-                        echo "PUTVAL \"$INSTANCE_ID/aicache/gauge-total_bytes_in\" interval=10 N:$STAT"
+                        echo "PUTVAL \"$INSTANCE_UUID/aicache/gauge-total_bytes_in\" interval=10 N:$STAT"
                 elif [ "$PREV_STAT" == "BOB:"   ] ; then
-                        echo "PUTVAL \"$INSTANCE_ID/aicache/gauge-total_bytes_out\" interval=10 N:$STAT"
+                        echo "PUTVAL \"$INSTANCE_UUID/aicache/gauge-total_bytes_out\" interval=10 N:$STAT"
                 fi
                 PREV_STAT="$STAT"
         done
